66ca6cbcad22a31bceba9f168707816af32cc2b2
linhdt <linh626688@gmail.com>
2018-10-10 00:32:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import axios from 'axios';
+
+export default class PersonList extends React.Component {
+    state = {
+        ads: []
+    }
+
+
+    componentDidMount() {
+        axios.get(`http://localhost:8088/getAllAds`)
+            .then(res => {
+                const ads = res.data;
+                this.setState({ ads: ads });
+                // console.log(ads[0].fullName);
+            })
+    }
+
+    render() {
+        return (
+            <div>
+
+
+                <LineChart width={1000} height={900} data={this.state.ads}
+                           margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                    <XAxis dataKey="creatTime"/>
+                    <YAxis/>
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <Tooltip/>
+                    <Legend/>
+                    <Line type="monotone" dataKey="countTotalShow" stroke="#8884d8" activeDot={{r: 8}}/>
+                    {/*<Line type="monotone" dataKey="totalTime" stroke="#82ca9d"/>*/}
+                </LineChart>
+            </div>
+        )
+    }
 }
-
-export default App;
