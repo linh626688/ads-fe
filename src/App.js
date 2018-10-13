463aa287c8b92236e87c09f6976059fc1ebe75cb
linhdt <linh626688@gmail.com>
2018-10-13 14:44:01 +0700

    update pie

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart, Bar } from 'recharts';
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart, Bar , PieChart,Pie } from 'recharts';
 // import { Row, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 
@@ -27,12 +27,19 @@ class Ads extends Component {
                 <BarChart width={600} height={300} data={this.state.ads}
                           margin={{top: 5, right: 30, left: 20, bottom: 5}}>
                     <CartesianGrid strokeDasharray="3 3"/>
-                    <XAxis dataKey="nameOS"/>
+                    <XAxis dataKey="name"/>
                     <YAxis/>
                     <Tooltip/>
                     <Legend />
-                    <Bar dataKey="countOfOs" fill="#8884d8" />
+                    <Bar dataKey="value" fill="#8884d8" />
                 </BarChart>
+
+
+                <PieChart width={800} height={400}>
+                    <Pie isAnimationActive={false} data={this.state.ads} cx={200} cy={200} outerRadius={80} fill="#8884d8" label/>
+                    <Pie data={this.state.ads} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
+                    <Tooltip/>
+                </PieChart>
             </div>
         )
     }
