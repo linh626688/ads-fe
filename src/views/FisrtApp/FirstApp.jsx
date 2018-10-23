19976c554cf59e5d86393f3bf77525402c50e786
linhdt <linhdt@gemvietnam.com>
2018-10-23 18:03:31 +0700

    test build

--- src/views/FisrtApp/FirstApp.jsx
+++ src/views/FisrtApp/FirstApp.jsx
@@ -1,108 +1,92 @@
-import React, { Component } from "react";
-// @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 import {
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  BarChart,
-  Bar,
-  PieChart,
-  Pie
-} from "recharts";
-import axios from "axios";
+    SortableContainer,
+    SortableElement,
+    SortableHandle,
+    arrayMove,
+} from 'react-sortable-hoc';
+import './spec.css'
 
-// core components
-import GridContainer from "components/Grid/GridContainer.jsx";
-
-const styles = {
-  cardCategoryWhite: {
-    "&,& a,& a:hover,& a:focus": {
-      color: "rgba(255,255,255,.62)",
-      margin: "0",
-      fontSize: "14px",
-      marginTop: "0",
-      marginBottom: "0"
-    },
-    "& a,& a:hover,& a:focus": {
-      color: "#FFFFFF"
-    }
-  },
-  cardTitleWhite: {
-    color: "#FFFFFF",
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none",
-    "& small": {
-      color: "#777",
-      fontSize: "65%",
-      fontWeight: "400",
-      lineHeight: "1"
-    }
-  }
+const pStyle = {
+    fontSize: '15px',
+    textAlign: 'center'
 };
 
+
 class FirstApp extends Component {
-  constructor() {
-    super();
-    this.state = {
-      ads: []
+    state = {
+        items: [
+            {
+                "name": "Doe",
+                "value": "1"
+            },
+            {
+                "name": "Doe2",
+                "value": "2"
+            },
+            {
+                "name": "Doe3",
+                "value": "3"
+            },
+            {
+                "name": "Doe4",
+                "value": "4"
+            }],
+        name: '',
+
     };
-  }
+    onSortEnd = ({oldIndex, newIndex}) => {
+        const {items} = this.state;
 
-  componentDidMount() {
-    axios.get(`http://35.185.80.237:8090/countByOs`).then(res => {
-      const ads = res.data;
-      this.setState({ ads: ads });
-      // console.log(ads[0].fullName);
-    });
-  }
+        this.setState({
+            items: arrayMove(items, oldIndex, newIndex),
+        });
+        console.log(this.state)
+    };
 
-  render() {
-    return (
-      <GridContainer>
-        <BarChart
-          width={600}
-          height={300}
-          data={this.state.ads}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="value" fill="#8884d8" />
-        </BarChart>
+    handleChange = (index) => (e)=> {
+        let items= this.state.items;
+        items[index] = {...items[index], value: e.target.value }
+       this.setState({items: items});
+        console.log(this.state)
+    }
 
-        <PieChart width={800} height={400}>
-          <Pie
-            isAnimationActive={false}
-            data={this.state.ads}
-            cx={200}
-            cy={200}
-            outerRadius={80}
-            fill="#8884d8"
-            label
-          />
-          <Pie
-            data={this.state.ads}
-            cx={500}
-            cy={200}
-            innerRadius={40}
-            outerRadius={80}
-            fill="#82ca9d"
-          />
-          <Tooltip />
-        </PieChart>
-      </GridContainer>
-    );
-  }
+
+    render() {
+        const {items, name} = this.state;
+        const DragHandle = SortableHandle(() =>
+            <span
+                className='unselectable'
+            >====</span>); // This can be any component you want
+
+        const SortableItem = SortableElement(({itemIndex, value}) => {
+            return (
+                <div>
+                    {value.name}
+                    <input
+                        name={itemIndex}
+                        id={`id_${itemIndex}`}
+                        onChange={this.handleChange(itemIndex)}
+                        value={value.value}
+                    />
+                    <DragHandle/>
+                </div>
+            );
+        });
+
+        const SortableList = SortableContainer(({items}) => {
+            return (
+                <ul>
+                    {items.map((value, index) => (
+                        <SortableItem key={`item-${index}`} index={index} itemIndex={index} value={value}/>
+                    ))}
+                </ul>
+            );
+        });
+
+        return <SortableList items={items} onSortEnd={this.onSortEnd} useDragHandle={true}/>;
+    }
 }
 
-export default withStyles(styles)(FirstApp);
+export default FirstApp;
